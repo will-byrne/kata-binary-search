6d71c56b280b395458dd63e34c4c5caea3b90c99
Will Byrne <w.byrne@elifesciences.org>
2020-03-23 13:54:45 +0000

    add single value array search

--- chop.js
+++ chop.js
@@ -1,7 +1,33 @@
 //chop(int, array_of_int)  -> int
 
 function chop(value, collection) {
-  return -1;
+  if (collection.length === 1 && collection[0] === value) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
 console.assert(chop(5, []) === -1);
+console.assert(chop(5, [3]) === -1);
+console.assert(chop(1, [1]) === 0);
+
+// assert_equal(0,  chop(1, [1]))
+//  #
+//  assert_equal(0,  chop(1, [1, 3, 5]))
+//  assert_equal(1,  chop(3, [1, 3, 5]))
+//  assert_equal(2,  chop(5, [1, 3, 5]))
+//  assert_equal(-1, chop(0, [1, 3, 5]))
+//  assert_equal(-1, chop(2, [1, 3, 5]))
+//  assert_equal(-1, chop(4, [1, 3, 5]))
+//  assert_equal(-1, chop(6, [1, 3, 5]))
+//  #
+//  assert_equal(0,  chop(1, [1, 3, 5, 7]))
+//  assert_equal(1,  chop(3, [1, 3, 5, 7]))
+//  assert_equal(2,  chop(5, [1, 3, 5, 7]))
+//  assert_equal(3,  chop(7, [1, 3, 5, 7]))
+//  assert_equal(-1, chop(0, [1, 3, 5, 7]))
+//  assert_equal(-1, chop(2, [1, 3, 5, 7]))
+//  assert_equal(-1, chop(4, [1, 3, 5, 7]))
+//  assert_equal(-1, chop(6, [1, 3, 5, 7]))
+// assert_equal(-1, chop(8, [1, 3, 5, 7]))
